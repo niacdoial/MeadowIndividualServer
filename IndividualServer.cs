35193158db2aeab17bebaf646f95e6c97328f4c3
invalidunits <invalidunits@gmail.com>
2025-08-08 13:39:20 -0400

    Initial Commit

---
+++ IndividualServer.cs
@@ -0,0 +1,11 @@
+﻿
+namespace RainMeadow.Server
+{
+    static class Application
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
